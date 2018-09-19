ab8b5dd4a144f330af013a3f3496dea61eeae56d
Taiki Takashita <taiki.takashita@qdenbs.com>
2018-09-19 14:34:25 +0900

    GoForwardUnityChanプロジェクト Lesson7「2Dゲームアプリを作ろう」の課題提出
    Lesson7「2Dゲームアプリを作ろう」の課題にて作成したファイルです。

---
+++ Assets/CubeController.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CubeController : MonoBehaviour
+{
+
+    // キューブの移動速度
+    private float speed = -0.2f;
+
+    // 消滅位置
+    private float deadLine = -10;
+
+    // Use this for initialization
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // キューブを移動させる
+        transform.Translate(this.speed, 0, 0);
+
+        // 画面外に出たら破棄する
+        if (transform.position.x < this.deadLine)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    //衝突時に呼ばれる関数
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        // Cubeか地面に接触したときにはボリュームを1にする（追加）
+        if (other.gameObject.tag == "CubePrefabTag" || other.gameObject.tag == "GroundTag")
+        {
+            GetComponent<AudioSource>().Play(0);
+        }
+    }
+}
